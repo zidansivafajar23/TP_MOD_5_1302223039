f4850b6f120c95eb3de4dc74a3c15d827cefc49b
zidan <zidansivafajar23@gmail.com>
2024-03-17 22:29:27 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223039/Program.cs
+++ tpmodul5_1302223039/Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿
+public class HaloGeneric
+{
+    public void SapaUser<U>(U user)
+    {
+        Console.WriteLine($"Halo {user}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T Data;
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<String>("Zidan");
+
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("1302223039");
+        dataGeneric.PrintData();
     }
 }
