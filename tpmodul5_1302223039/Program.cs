53f1434f1a3bbc33c58266f03a9ae4f21198ff37
zidan <zidansivafajar23@gmail.com>
2024-03-17 22:08:11 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_1302223039/Program.cs
+++ tpmodul5_1302223039/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿
+public class HaloGeneric
+{
+    public void SapaUser<U>(U user)
+    {
+        Console.WriteLine($"Halo {user}");
+    }
+}
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Membuat instance dari kelas HaloGeneric
+        HaloGeneric halo = new HaloGeneric();
+
+        // Memanggil metode SapaUser dengan menyediakan argumen string "Rakha"
+        halo.SapaUser<String>("Zidan");
     }
 }
--- tpmodul5_1302223039/obj/Debug/net8.0/tpmodul5_1302223039.AssemblyInfo.cs
+++ tpmodul5_1302223039/obj/Debug/net8.0/tpmodul5_1302223039.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302223039")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20887f81216f2217747c9baabfd1bbc33953ac3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302223039")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302223039")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
